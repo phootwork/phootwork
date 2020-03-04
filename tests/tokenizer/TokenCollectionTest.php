9ee1f2c9b15cbb2f45e7acab67cad7637881bba9
Cristiano Cinotti <cristianocinotti@gmail.com>
2020-03-04 18:08:45 +0100

    Fix TokenCollection
    Adjust `TokenCollection::get()` method to return null, for consistency
    with `ArrayList::get`.

---
+++ tests/tokenizer/TokenCollectionTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Phootwork package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ * @copyright Thomas Gossmann
+ */
+namespace phootwork\tokenizer\tests;
+
+use phootwork\tokenizer\PhpTokenizer;
+use phootwork\tokenizer\Token;
+
+class TokenCollectionTest extends TokenizerTest {
+	public function testGetMethod(): void {
+		$sample = $this->getSample('class');
+
+		$tokenizer = new PhpTokenizer();
+		$tokens = $tokenizer->tokenize($sample);
+
+		$this->assertEquals(99, $tokens->size(), 'The fixture class has 99 tokens');
+		$this->assertInstanceOf(Token::class, $tokens->get(55));
+		$this->assertNull($tokens->get(500));
+	}
+}
